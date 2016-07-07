3e17bba11de8ba3dd171f85c0822cca652ff9c27
Tomasz Panek <joachimsantana@gmail.com>
2016-07-07 19:24:44 +0200

    Annotate all models

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: currencies
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  converter   :integer
+#  code        :string(255)
+#  buy_price   :float
+#  sell_price  :float
+#  exchange_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
