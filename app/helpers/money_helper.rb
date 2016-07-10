d4d2cb5b6778c8c9baecde89614f259baccfa363
Tomasz Panek <joachimsantana@gmail.com>
2016-07-10 19:11:35 +0200

    Small median improvment - add round(4)

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -2,7 +2,7 @@ module MoneyHelper
   def median(array)
     sorted = array.sort
     len = sorted.length
-    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+    ((sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0).round(4)
   end
 
   def average(array)
